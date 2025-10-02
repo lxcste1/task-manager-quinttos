0120b3a78ecfd82dfacfdfa5262bf145e1b8f534
lxcste <lxcste@gmail.com>
2025-10-02 02:10:00 +0000

    feat:(frontend): add register logic

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,2 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-
-Route::post('/api/login',  [AuthController::class, 'login']);
-Route::post('/api/logout', [AuthController::class, 'logout']);
+// routes/web.php
